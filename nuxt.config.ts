f066f6c4c97dbd3a85a48559e4c3c906d9bc220b
Turdiyev <sherbekturdiyev98@gmail.com>
2024-07-26 11:36:02 +0500

    get error info api nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["vuetify-nuxt-module"],
-  css: ['@/assets/style/main.css'],
+  // modules: ["vuetify-nuxt-module"],
+  css: ["vuetify-nuxt-module", '@/assets/style/main.css'],
+  modules: ['@nuxtjs/axios'],
+  axios: {
+    baseURL: 'http://localhost:3001', // Sizning API serveringiz URL manzili
+  },
+  server: {
+    port: 3000, // default: 3000
+    host: '0.0.0.0' // localhost o'rniga barcha interfeyslardan kirish
+  },
+  head: {
+    meta: [
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ]
+  }
 })
